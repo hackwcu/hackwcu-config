af3edd12bab15f6740a361e72574e0cff6f53cbe
Brandon Barker <contact@brandonbarker.me>
2020-01-19 21:09:40 -0500

    Update comments

--- assets/javascripts/custom.js
+++
@@ -1,9 +0,0 @@
-// Custom javascript code which is run on every page.
-
-var init = function() {
-    console.log("This is custom javascript run on every pageload.");
-}
-
-window.addEventListener('load', init);
-$(document).on('turbolinks:load', init);
-
